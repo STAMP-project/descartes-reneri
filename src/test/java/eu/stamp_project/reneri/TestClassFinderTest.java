49a28aa824f1d4d93bede782dd289b564ce7e223
oscarlvp <oscarlvp@gmail.com>
2019-01-14 12:11:08 +0100

    Simplifying TestClassFinder tests by not using resources

--- src/test/java/eu/stamp_project/reneri/TestClassFinderTest.java
+++ src/test/java/eu/stamp_project/reneri/TestClassFinderTest.java
@@ -11,34 +11,63 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TestClassFinderTest {
-
-
-    private Optional<CtClass<?>> getClassFromResources(String name) {
-        Launcher launcher = new Launcher();
-        launcher.addInputResource(new ResourceJavaFile(String.format("sources/%s.java", name)));
-        CtModel model = launcher.buildModel();
-        return model.getElements((CtClass<?> type) -> true).stream().findFirst();
-    }
-
     @Test
     public void testFinJUnit3TestClass() {
-        Optional<CtClass<?>> theClass = getClassFromResources("JUnit3TestClass");
-        assertTrue(theClass.isPresent());
-        assertTrue("It is not a JUnit 3 class as expected", TestClassFinder.isJunit3TestClass(theClass.get()));
+        CtClass<?> theClass = Launcher.parseClass(
+                "package sources;\n" +
+                "\n" +
+                "\n" +
+                "import junit.framework.TestCase;\n" +
+                "\n" +
+                "public class JUnit3TestClass extends TestCase {\n" +
+                "\n" +
+                "    public void tearUp() {}\n" +
+                "\n" +
+                "    public void tearDown() {}\n" +
+                "\n" +
+                "    public void testSomething() {\n" +
+                "        fail();\n" +
+                "    }\n" +
+                "\n" +
+                "}"
+        );
+        assertTrue("It is not a JUnit 3 class as expected", TestClassFinder.isJunit3TestClass(theClass));
     }
 
     @Test
     public void testFinJUnit4TestClass() {
-        Optional<CtClass<?>> theClass = getClassFromResources("JUnit4TestClass");
-        assertTrue(theClass.isPresent());
-        assertTrue("It is not a JUnit 4 class", TestClassFinder.isJunit4TestClass(theClass.get()));
+        CtClass<?> theClass = Launcher.parseClass(
+                "package sources;\n" +
+                        "\n" +
+                        "import org.junit.Test\n" +
+                        "\n" +
+                        "public class JUnit4TestClass {\n" +
+                        "\n" +
+                        "    @Test\n" +
+                        "    public void test() {\n" +
+                        "\n" +
+                        "    }\n" +
+                        "\n" +
+                        "}"
+        );
+        assertTrue("It is not a JUnit 4 class", TestClassFinder.isJunit4TestClass(theClass));
     }
 
     @Test
     public void testNotATest() {
-        Optional<CtClass<?>> theClass = getClassFromResources("NoTest");
-        assertTrue(theClass.isPresent());
-        assertFalse("Should not be a test class", TestClassFinder.isTestClass(theClass.get()));
+
+        CtClass<?> theClass = Launcher.parseClass(
+          "package sources;\n" +
+                  "\n" +
+                  "public class NoTest {\n" +
+                  "\n" +
+                  "    public boolean isTest() {\n" +
+                  "        return false;\n" +
+                  "    }\n" +
+                  "\n" +
+                  "}"
+        );
+        assertFalse("Should not be a test class", TestClassFinder.isTestClass(theClass));
     }
 
 }
--- src/test/resources/sources/NoTest.java
+++
@@ -1,9 +0,0 @@
-package sources;
-
-public class NoTest {
-
-    public boolean isTest() {
-        return false;
-    }
-
-}
