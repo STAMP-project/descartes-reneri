c9082512bccf991446159c53ce7af0c5c5a2fa33
oscarlvp <oscarlvp@gmail.com>
2019-03-11 09:57:13 +0100

    Added a new test case combining TestCase and @Test in a static method

--- src/test/java/eu/stamp_project/reneri/TestClassFinderTest.java
+++ src/test/java/eu/stamp_project/reneri/TestClassFinderTest.java
@@ -15,21 +15,21 @@ public class TestClassFinderTest {
     public void testFinJUnit3TestClass() {
         CtClass<?> theClass = Launcher.parseClass(
                 "package sources;\n" +
-                "\n" +
-                "\n" +
-                "import junit.framework.TestCase;\n" +
-                "\n" +
-                "public class JUnit3TestClass extends TestCase {\n" +
-                "\n" +
-                "    public void tearUp() {}\n" +
-                "\n" +
-                "    public void tearDown() {}\n" +
-                "\n" +
-                "    public void testSomething() {\n" +
-                "        fail();\n" +
-                "    }\n" +
-                "\n" +
-                "}"
+                        "\n" +
+                        "\n" +
+                        "import junit.framework.TestCase;\n" +
+                        "\n" +
+                        "public class JUnit3TestClass extends TestCase {\n" +
+                        "\n" +
+                        "    public void tearUp() {}\n" +
+                        "\n" +
+                        "    public void tearDown() {}\n" +
+                        "\n" +
+                        "    public void testSomething() {\n" +
+                        "        fail();\n" +
+                        "    }\n" +
+                        "\n" +
+                        "}"
         );
         assertTrue("It is not a JUnit 3 class as expected", TestClassFinder.isJunit3TestClass(theClass));
     }
@@ -57,17 +57,35 @@ public class TestClassFinderTest {
     public void testNotATest() {
 
         CtClass<?> theClass = Launcher.parseClass(
-          "package sources;\n" +
-                  "\n" +
-                  "public class NoTest {\n" +
-                  "\n" +
-                  "    public boolean isTest() {\n" +
-                  "        return false;\n" +
-                  "    }\n" +
-                  "\n" +
-                  "}"
+                "package sources;\n" +
+                        "\n" +
+                        "public class NoTest {\n" +
+                        "\n" +
+                        "    public boolean isTest() {\n" +
+                        "        return false;\n" +
+                        "    }\n" +
+                        "\n" +
+                        "}"
         );
         assertFalse("Should not be a test class", TestClassFinder.isTestClass(theClass));
     }
+
+    @Test
+    public void testMixedCaseWithStatic() {
+
+        CtClass<?> testClass = Launcher.parseClass(
+                "package source;\n" +
+                        "import junit.framework.TestCase;\n" +
+                        "import org.junit.Test;\n\n" +
+                        "@SupressWarnings(\"javadoc\")"+
+                        "public final class ExampleTest extends TestCase {\n" +
+                            "@Test\n" +
+                            "public static void testExample() throws Exception { fail(); }\n" +
+                        "}");
+        assertTrue("Should be a test class anyways", TestClassFinder.isTestClass(testClass));
+
+
+    }
+
 
 }
