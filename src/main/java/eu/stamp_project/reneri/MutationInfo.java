bcede01fa9e08b663d9f2026041c74157240bb78
oscarlvp <oscarlvp@gmail.com>
2019-03-11 09:53:46 +0100

    Adding equals and hash code to MuationInfo

--- src/main/java/eu/stamp_project/reneri/MutationInfo.java
+++ src/main/java/eu/stamp_project/reneri/MutationInfo.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonObject;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Objects;
 
 public class MutationInfo {
 
@@ -38,7 +39,28 @@ public class MutationInfo {
 
     }
 
+    public String getClassQualifiedName() {
+        return packageName + "." + className;
+    }
+
     public boolean matches(String name, String desc) {
         return methodName.equals(name) && methodDescription.equals(desc);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MutationInfo that = (MutationInfo) o;
+        return Objects.equals(mutator, that.mutator) &&
+                Objects.equals(className, that.className) &&
+                Objects.equals(packageName, that.packageName) &&
+                Objects.equals(methodName, that.methodName) &&
+                Objects.equals(methodDescription, that.methodDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mutator, className, packageName, methodName, methodDescription);
     }
 }
